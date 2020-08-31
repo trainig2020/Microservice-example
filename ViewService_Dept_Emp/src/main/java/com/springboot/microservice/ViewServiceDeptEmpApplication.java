32863fbbdda3a3ef23f10fc3d8975ff4d9b6dae3
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-08-31 18:13:12 +0530

    Add files via upload

---
+++ ViewService_Dept_Emp/src/main/java/com/springboot/microservice/ViewServiceDeptEmpApplication.java
@@ -0,0 +1,21 @@
+package com.springboot.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ViewServiceDeptEmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewServiceDeptEmpApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
